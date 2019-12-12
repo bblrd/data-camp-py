f7a382807268fde86351eff6234f415e28747d36
Brad Ballard <ballard6@gmail.com>
2019-12-12 12:30:19 -0500

    Added file 19

---
+++ dc19_intro-shell-ds.py
@@ -0,0 +1,9 @@
+# Introduction to Shell for Data Science on DataCamp
+
+#######################################
+
+# Part 1: Manipulating files and directories
+
+#######################################
+
+## Manipulating files and directories
